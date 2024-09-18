03c7f980b7aeab4ed4a4ce91af848c8ddf769b1e
Troy Grossi <ttroygrossit@gmail.com>
2024-09-17 18:45:35 -0700

    added ssl server options, changed env to production

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const dotenv = require('dotenv');
 const axios = require('axios');
 const path = require('path')
 const routes = require('./api')
+// 
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 
 dotenv.config();
 
@@ -19,10 +23,35 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 // 
 app.use(routes)
 
-const PORT = process.env.PORT || 5000;
 
+if(process.env.NODE_ENV === "development"){
+  const PORT = process.env.PORT_HTTP;
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}else{
 
+// SSL certificate options
+const sslOptions = {
+  key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/privkey.pem`),
+  cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/fullchain.pem`)
+};
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Listen on port 80 for HTTP and redirect to HTTPS
+http.createServer((req, res) => {
+  res.writeHead(301, { "Location": `https://${req.headers.host}${req.url}` });
+  res.end();
+}).listen(process.env.PORT_HTTP || 80, () => {
+  console.log('HTTP server running and redirecting to HTTPS');
 });
+
+// Listen on port 443 for HTTPS
+https.createServer(sslOptions, app).listen(process.env.PORT_HTTPS || 443, () => {
+  console.log('HTTPS server running on port 443');
+});
+}
+
+
+
+
+
